856253e10ba6f94ec0a2920583b028870e7d5f15
Alvin <75377410+Zora24@users.noreply.github.com>
2021-01-18 17:14:21 +0700

    Update lordanimasi.py

--- userbot/modules/lordanimasi.py
+++ userbot/modules/lordanimasi.py
@@ -35,8 +35,8 @@ async def get_readable_time(seconds: int) -> str:
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 
 
-@register(outgoing=True, pattern="^.impostor$")
-async def koc(e):
+@bot.on(events.NewMessage(pattern=r"\.impostor(.*)", outgoing=True))
+async def _(event):
     if event.fwd_from:
         return
     hmm = bot.uid
@@ -83,72 +83,72 @@ async def koc(e):
         event.chat_id, "CAADAQADLwADnjOcH-wxu-ehy6NRAg"
     )
     event = await event.reply(f"{name} is ejected.......")
-    await asyncio.sleep(2)
+    await sleep(2)
     await event.edit("ඞㅤㅤㅤㅤ ㅤㅤㅤㅤ")
-    await asyncio.sleep(0.5)
+    await sleep(0.5)
     await event.edit("ㅤඞㅤㅤㅤㅤ ㅤㅤㅤ")
-    await asyncio.sleep(0.5)
+    await sleep(0.5)
     await event.edit("ㅤㅤ ඞㅤㅤㅤㅤㅤㅤ")
-    await asyncio.sleep(0.5)
+    await sleep(0.5)
     await event.edit("ㅤㅤㅤ ඞㅤㅤㅤㅤㅤ")
-    await asyncio.sleep(0.5)
+    await sleep(0.5)
     await event.edit("ㅤㅤㅤㅤ ඞㅤㅤㅤㅤ")
-    await asyncio.sleep(0.5)
+    await sleep(0.5)
     await event.edit("ㅤㅤㅤㅤㅤ ඞㅤㅤㅤ")
-    await asyncio.sleep(0.5)
+    await sleep(0.5)
     await event.edit("ㅤㅤㅤㅤㅤㅤ ඞㅤㅤ")
-    await asyncio.sleep(0.5)
+    await sleep(0.5)
     await event.edit("ㅤㅤㅤㅤㅤㅤㅤ ඞㅤ")
-    await asyncio.sleep(0.5)
+    await sleep(0.5)
     await event.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ඞ")
-    await asyncio.sleep(0.5)
+    await sleep(0.5)
     await event.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ㅤ")
-    await asyncio.sleep(0.2)
+    await sleep(0.2)
     await stcr4.delete()
     if cmd == "":
         await event.edit(
             f". 　　　。　　　　•　 　ﾟ　　。 　　.\n .　　　 　　.　　　　　。　　 。　. 　\n\n  . 　　 。   　     ඞ         。 . 　　 • 　　　　•\n\n  ﾟ{name} was an Imposter.      。　. 　 　       。　.                                        。　. \n                                   　.          。　  　. \n　'         0 Impostor remains    　 。　.  　　.                。　.        。 　     .          。 　            .               .         .    ,      。\n　　ﾟ　　　.　　.    ,　 　。　 　. 　 .     。"
         )
-        await asyncio.sleep(4)
+        await sleep(4)
         await event.delete()
         await event.client.send_file(event.chat_id, "CAADAQADLQADnjOcH39IqwyR6Q_0Ag")
     elif cmd == "n":
         await event.edit(
             f". 　　　。　　　　•　 　ﾟ　　。 　　.\n .　　　 　　.　　　　　。　　 。　. 　\n\n  . 　　 。   　     ඞ         。 . 　　 • 　　　　•\n\n  ﾟ{name} was not an Imposter.      。　. 　 　       。　.                                        。　. \n                                   　.          。　  　. \n　'         1 Impostor remains    　 。　.  　　.                。　.        。 　     .          。 　            .               .         .    ,      。\n　　ﾟ　　　.　　.    ,　 　。　 　. 　 .     。"
         )
-        await asyncio.sleep(4)
+        await sleep(4)
         await event.delete()
         await event.client.send_file(event.chat_id, "CAADAQADQAADnjOcH-WOkB8DEctJAg")
 
 
-@register(outgoing=True, pattern="^.impos$")
-async def koc(e):
+@bot.on(events.NewMessage(pattern=r"\.impos(.*)", outgoing=True))
+async def _(event):
     if event.fwd_from:
         return
     name = event.pattern_match.group(2)
     cmd = event.pattern_match.group(1).lower()
     event = await edit_or_reply(event, f"{name} is ejected.......")
-    await asyncio.sleep(2)
+    await sleep(2)
     await event.edit("ඞㅤㅤㅤㅤ ㅤㅤㅤㅤ")
-    await asyncio.sleep(0.8)
+    await sleep(0.8)
     await event.edit("ㅤඞㅤㅤㅤㅤ ㅤㅤㅤ")
-    await asyncio.sleep(0.8)
+    await sleep(0.8)
     await event.edit("ㅤㅤ ඞㅤㅤㅤㅤㅤㅤ")
-    await asyncio.sleep(0.8)
+    await sleep(0.8)
     await event.edit("ㅤㅤㅤ ඞㅤㅤㅤㅤㅤ")
-    await asyncio.sleep(0.8)
+    await sleep(0.8)
     await event.edit("ㅤㅤㅤㅤ ඞㅤㅤㅤㅤ")
-    await asyncio.sleep(0.8)
+    await sleep(0.8)
     await event.edit("ㅤㅤㅤㅤㅤ ඞㅤㅤㅤ")
-    await asyncio.sleep(0.8)
+    await sleep(0.8)
     await event.edit("ㅤㅤㅤㅤㅤㅤ ඞㅤㅤ")
-    await asyncio.sleep(0.8)
+    await sleep(0.8)
     await event.edit("ㅤㅤㅤㅤㅤㅤㅤ ඞㅤ")
-    await asyncio.sleep(0.8)
+    await sleep(0.8)
     await event.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ඞ")
-    await asyncio.sleep(0.8)
+    await sleep(0.8)
     await event.edit("ㅤㅤㅤㅤㅤㅤㅤㅤ ㅤ")
-    await asyncio.sleep(0.2)
+    await sleep(0.2)
     if cmd == "":
         await event.edit(
             f". 　　　。　　　　•　 　ﾟ　　。 　　.\n .　　　 　　.　　　　　。　　 。　. 　\n\n  . 　　 。   　     ඞ         。 . 　　 • 　　　　•\n\n  ﾟ {name} was an Imposter.      。　. 　 　       。　.                                        。　. \n                                   　.          。　  　. \n　'         0 Impostor remains    　 。　.  　　.                。　.        。 　     .          。 　            .               .         .    ,      。\n　　ﾟ　　　.　　.    ,　 　。　 　. 　 .     。"
@@ -159,13 +159,15 @@ async def koc(e):
         )
 
 
-@register(outgoing=True, pattern="^.lordping$")
-async def pingme(pong):
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
     animation_interval = 0.2
     animation_ttl = range(26)
+    input_str = event.pattern_match.group(1)
+    if input_str == "lordping":
     event = await edit_or_reply(event, "`Ping....`")
     animation_chars = [
         "⬛⬛⬛⬛⬛⬛⬛⬛⬛",
